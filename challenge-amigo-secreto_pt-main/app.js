aa7621901c301fb98a090859c7e57473d0f75d3c
eduardoDeAAMelo <eduardo.melo0802@gmail.com>
2025-01-27 12:00:51 -0300

    Desafio Completado

--- challenge-amigo-secreto_pt-main/app.js
+++ challenge-amigo-secreto_pt-main/app.js
@@ -1,1 +1,37 @@
-//O principal objetivo deste desafio é fortalecer suas habilidades em lógica de programação. Aqui você deverá desenvolver a lógica para resolver o problema.
+let amigos = [];
+
+function adicionarAmigo(){
+    
+    let nomesDosAmigos = document.getElementById("amigo").value;
+        if(nomesDosAmigos == ""){
+            alert("Por favor, insira um nome");
+
+    } else {
+        amigos.push(nomesDosAmigos);
+        exibirNomesNaTela();
+        limparCampo();
+    }
+    
+}
+
+function limparCampo(){
+    nomesDosAmigos = document.querySelector("input");
+    nomesDosAmigos.value = "";
+
+}
+
+
+function exibirNomesNaTela(){
+    exibirNomes = document.getElementById("listaAmigos");
+    exibirNomes.innerHTML = amigos;
+    
+}
+
+function sortearAmigo(){
+    let nomeSorteado = document.getElementById("resultado");
+    let gerarSorteio = amigos[Math.floor(Math.random() * amigos.length)]
+
+            alert(`O nome sorteado foi ${gerarSorteio}!`)
+
+        nomeSorteado.innerHTML = gerarSorteio;
+}
